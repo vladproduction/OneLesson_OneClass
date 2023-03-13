7a2e260867a0033d93bf652858884765b810cab1
VladBogda <vladbogdantsev@gmail.com>
2023-03-13 16:46:51 +0200

    deep clone() logic modified

--- OneLesson_OneClass/src/main/java/Clone/app03/Main.java
+++ OneLesson_OneClass/src/main/java/Clone/app03/Main.java
@@ -7,41 +7,63 @@ public class Main {
         a.setName("bmw");
         a.setModel("x6");
         a.setColor("black");
-        Equipment full = new Equipment();
-        a.setEquipment(full);
         a.setPrice(50000);
 
-        full.setTypeEngine("D");
-        full.setEngineCapacity(3.5);
+        Equipment Diesel = new Equipment();
+        Diesel.setTypeEngine("Diesel");
+        Diesel.setEngineCapacity(3.5);
+        a.setEquipment(Diesel);
 
         Car b = a.clone();
-
+        System.out.println("Total clone:");
         System.out.println(a);
         System.out.println(b);
         System.out.println(a.hashCode());
         System.out.println(b.hashCode());
-//        System.out.println("update for car b type of engine --> full_ECO");
-//        System.out.println("update for car b engine capacity --> 2.5");
-//        Equipment ECO = new Equipment();
-//        ECO.setTypeEngine("full_ECO");
-//        ECO.setEngineCapacity(2.5);
-//
-//        b.setEquipment(ECO);
-//        System.out.println(a);
-//        System.out.println(b);
-//        System.out.println(a.hashCode());
-//        System.out.println(b.hashCode());
+        System.out.println();
 
-        System.out.println("-----------------------------------------");
-        System.out.println("Version 2.0 = update for car b type of engine --> full_ECO");
+        System.out.println("===Equipment ECO: ");
+        System.out.println("update for car b type of engine --> ECO");
+        System.out.println("update for car b engine capacity --> 2.5");
         Equipment ECO = new Equipment();
-        ECO.setTypeEngine("full_ECO");
-        //??????????? why we have to set engineCapacity again for
+        ECO.setTypeEngine("ECO");
+        ECO.setEngineCapacity(2.5);
         b.setEquipment(ECO);
+
         System.out.println(a);
         System.out.println(b);
         System.out.println(a.hashCode());
         System.out.println(b.hashCode());
+        System.out.println();
+
+        System.out.println("===Equipment Fuel: ");
+        System.out.println("update for car a type of engine --> Fuel");
+        System.out.println("update for car a engine capacity --> 2.0");
+        Equipment Fuel = new Equipment();
+        Fuel.setTypeEngine("Fuel");
+        Fuel.setEngineCapacity(2.0);
+        a.setEquipment(Fuel);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+        System.out.println();
+
+        System.out.println("===Equipment bEquipment(special properties): ");
+        System.out.println("update for car b type of engine --> Diesel_ECO-5");
+        System.out.println("update for car b engine capacity --> 2.2");
+        Equipment bEquipment = b.getEquipment();
+        bEquipment.setTypeEngine("Diesel_ECO-5");
+        bEquipment.setEngineCapacity(2.2);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+
+
+
 
 
 
